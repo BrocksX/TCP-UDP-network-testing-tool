64ac01a6549430eef97289cb1d48f4491d30c3fb
BrocksX <91247160+BrocksX@users.noreply.github.com>
2023-02-05 21:51:19 +0800

    support multi tcp client

--- TCP_UDP_TOOL/aboutsoftware.cpp
+++ TCP_UDP_TOOL/aboutsoftware.cpp
@@ -6,6 +6,7 @@ AboutSoftware::AboutSoftware(QWidget *parent) :
     ui(new Ui::AboutSoftware)
 {
     ui->setupUi(this);
+    this->setWindowTitle("TCP/UDP Testing Tool");
     ui->author->setOpenExternalLinks(true);
     ui->author->setText("<a href = https://github.com/BrocksX>Author: https://github.com/BrocksX");
 }
